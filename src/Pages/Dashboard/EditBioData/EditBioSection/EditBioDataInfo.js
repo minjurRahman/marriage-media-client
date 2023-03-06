d67571a83052eb17e175e381530b4ae421e55c99
Minjur <minjur.rahaman99@gmail.com>
2023-03-07 00:06:14 +0600

    edit section display

--- src/Pages/Dashboard/EditBioData/EditBioSection/EditBioDataInfo.js
+++ src/Pages/Dashboard/EditBioData/EditBioSection/EditBioDataInfo.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Address from '../EditBioAllComponents/Address';
+import Commitment from '../EditBioAllComponents/Commitment';
+import Communication from '../EditBioAllComponents/Communication';
+import EducationalInfo from '../EditBioAllComponents/EducationalInfo';
+import ExpectedLifePartnerInfo from '../EditBioAllComponents/ExpectedLifePartnerInfo';
+import FamilyInfo from '../EditBioAllComponents/FamilyInfo';
+import GeneralInfo from '../EditBioAllComponents/GeneralInfo';
+import MarriageRelatedInfo from '../EditBioAllComponents/MarriageRelatedInfo';
+import PersonalInfo from '../EditBioAllComponents/PersonalInfo';
+import ProfessionalInfo from '../EditBioAllComponents/ProfessionalInfo';
 
 const EditBioDataInfo = () => {
     return (
         <div>
-            Edit Bio Data Info
+            <GeneralInfo></GeneralInfo>
+            <Address></Address>
+            <EducationalInfo></EducationalInfo>
+            <FamilyInfo></FamilyInfo>
+            <PersonalInfo></PersonalInfo>
+            <ProfessionalInfo></ProfessionalInfo>
+            <MarriageRelatedInfo></MarriageRelatedInfo>
+            <ExpectedLifePartnerInfo></ExpectedLifePartnerInfo>
+            <Commitment></Commitment>
+            <Communication></Communication>
         </div>
     );
 };
